d3fddbf29acfbb8e61b03dc593fc5a2d6dfc4b47
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-12 00:26:52 +0300

    + Admin Template Added +

---
+++ Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaFestavaLive.Entities
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
