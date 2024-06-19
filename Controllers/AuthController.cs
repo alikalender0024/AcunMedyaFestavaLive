e07146fa8643a1cf32a13f485dc68761269f40c8
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-20 02:30:49 +0300

    + Login and Register Operation Completed

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using AcunMedyaFestavaLive.DataAccess.Context;
+using AcunMedyaFestavaLive.Entities;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AcunMedyaFestavaLive.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly FestavaContext _festavaContext = new FestavaContext();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _festavaContext.Users.Add(user);
+                _festavaContext.SaveChanges();
+
+
+                var userOperationClaim = new UserOperationClaim
+                {
+                    UserId = user.UserID,
+                    OperationClaimId = 1
+                };
+                _festavaContext.UserOperationClaims.Add(userOperationClaim);
+                _festavaContext.SaveChanges();
+
+                return RedirectToAction("Index", "Default");
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = _festavaContext.Users.SingleOrDefault(u => u.UserName == username && u.Password == password);
+            if (user != null)
+            {
+                Session["UserID"] = user.UserID;
+                Session["UserName"] = user.UserName;
+
+                return RedirectToAction("TicketList", "Ticket", new { area = "Admin" });
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid login attempt.";
+                return View();
+            }
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Auth");
+        }
+    }
+}
---
+++ DataAccess/UserDal.cs
@@ -0,0 +1,30 @@
+﻿using AcunMedyaFestavaLive.DataAccess.Context;
+using AcunMedyaFestavaLive.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaFestavaLive.DataAccess
+{
+    public class UserDal
+    {
+        FestavaContext _festavaContext = new FestavaContext();
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+
+            var result = from userOperationClaim in _festavaContext.UserOperationClaims
+                         join operationClaim in _festavaContext.OperationClaims
+                         on userOperationClaim.OperationClaimId equals operationClaim.OperationClaimID
+                         where userOperationClaim.UserId==user.UserID
+                         select new OperationClaim
+                         {
+                             OperationClaimID = operationClaim.OperationClaimID,
+                             ClaimName=operationClaim.ClaimName
+                         };
+            return result.ToList();
+
+        }
+    }
+}
